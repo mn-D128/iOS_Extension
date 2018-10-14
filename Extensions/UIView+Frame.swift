c6ed5b7006acc7492a15d78e509a3c80d8d978a0
mn(D128) <>
2018-10-14 15:34:00 +0900

    UIViewのframe用extensionの追加

---
+++ Extensions/UIView+Frame.swift
@@ -0,0 +1,36 @@
+//
+//  UIView+Frame.swift
+//
+//  Created by mn(D128) on 2018/10/14.
+//  Copyright © 2018年 mn(D128). All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+
+    var frameY: CGFloat {
+        set {
+            var frame: CGRect = self.frame
+            frame.origin.y = newValue
+            self.frame = frame
+        }
+        
+        get {
+            return self.frame.origin.y
+        }
+    }
+    
+    var frameHeight: CGFloat {
+        set {
+            var frame: CGRect = self.frame
+            frame.size.height = newValue
+            self.frame = frame
+        }
+        
+        get {
+            return self.frame.size.height
+        }
+    }
+
+}
