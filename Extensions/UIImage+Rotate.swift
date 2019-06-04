1354a4f73566deae3f1928d071645ca43444606e
nakan@D128 <>
2019-06-05 01:27:20 +0900

    計算誤差が発生したため90度回転固定にする

--- Extensions/UIImage+Rotate.swift
+++ Extensions/UIImage+Rotate.swift
@@ -11,11 +11,11 @@ import UIKit
 extension UIImage {
 
     // 参考：https://qiita.com/taji-taji/items/4f01273b303edec45d46
-    func rotatedBy(degree: CGFloat) -> UIImage? {
-        let radian: CGFloat = -degree * CGFloat.pi / 180.0
-        var rotatedRect: CGRect = CGRect(origin: .zero,
-                                         size: self.size)
-        rotatedRect = rotatedRect.applying(CGAffineTransform(rotationAngle: radian))
+    var rotate90Degree: UIImage? {
+        let radian: CGFloat = -90.0 * CGFloat.pi / 180.0
+        let rotatedRect: CGRect = CGRect(origin: .zero,
+                                         size: CGSize(width: self.size.height,
+                                                      height: self.size.width))
 
         UIGraphicsBeginImageContext(rotatedRect.size)
         guard let context: CGContext = UIGraphicsGetCurrentContext(),
@@ -24,14 +24,14 @@ extension UIImage {
             return nil
         }
         
-        context.translateBy(x: rotatedRect.size.width / 2,
-                            y: rotatedRect.size.height / 2)
+        context.translateBy(x: rotatedRect.size.width / 2.0,
+                            y: rotatedRect.size.height / 2.0)
         context.scaleBy(x: 1.0, y: -1.0)
         
         context.rotate(by: radian)
         context.draw(cgImage,
-                     in: CGRect(x: -(self.size.width / 2),
-                                y: -(self.size.height / 2),
+                     in: CGRect(x: -(self.size.width / 2.0),
+                                y: -(self.size.height / 2.0),
                                 width: self.size.width,
                                 height: self.size.height))
         
