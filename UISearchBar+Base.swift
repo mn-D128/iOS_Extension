5aeab7cacd2412b4ab19e2cfc6fabfb5e91915f4
nakan@D128 <>
2019-10-02 23:52:25 +0900

    UISearchBarからのTextField取得方法の変更

--- UISearchBar+Base.swift
+++ UISearchBar+Base.swift
@@ -11,6 +11,9 @@ import UIKit
 extension UISearchBar {
     
     var textField: UITextField? {
+        if #available(iOS 13.0, *) {
+            return self.searchTextField
+        }
         return value(forKey: "_searchField") as? UITextField
     }
     
